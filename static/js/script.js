ce393a204ccdf86aa3c3c47d8266e8b729468fa2
saimani251 <68836877+saimani251@users.noreply.github.com>
2024-04-25 13:49:42 -0400

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -9,12 +9,12 @@ function toggleMenu() {
 let slideIndex = 0;
 showSlides();
 
-// Next/previous controls
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -32,13 +32,13 @@ function showSlides() {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  if (slides.length > 0) { // Check if there are slides
+  if (slides.length > 0) { 
     slides[slideIndex-1].style.display = "block";  
   }
-  if (dots.length > 0) { // Check if there are dots
+  if (dots.length > 0) { 
     dots[slideIndex-1].className += " active";
   }
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  setTimeout(showSlides, 4000); 
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -46,15 +46,15 @@ document.addEventListener('DOMContentLoaded', function () {
     var bowlingSelect = document.getElementById('bowling_team');
 
     function handleTeamSelection(event, otherSelect) {
-        // Find all options in the other select
+       
         var otherOptions = otherSelect.options;
 
-        // Loop through all options in the other select element
+        
         for (var i = 0; i < otherOptions.length; i++) {
-            otherOptions[i].disabled = false; // Enable all options first
+            otherOptions[i].disabled = false; 
         }
 
-        // Disable the option in the other select that matches the selected value
+       
         if (event.target.value) {
             var optionToDisable = Array.from(otherOptions).find(option => option.value === event.target.value);
             if (optionToDisable) {
@@ -62,9 +62,9 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-        // Reset the other select if the currently selected team is now disabled
+        
         if (otherSelect.options[otherSelect.selectedIndex].disabled) {
-            otherSelect.value = ''; // Resets the select box if selected team is disabled
+            otherSelect.value = ''; 
         }
     }
 
